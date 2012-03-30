b401185891dcef4a57e5c8878f44988c504d233d
Lee <lee.j.cephas@gmail.com>
2012-03-30 10:55:07 -0500

    deleted/renamed files

--- PHP/ajaxTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("Connect.php");
-include("Updates.php");
-
-$count = $_GET['count']; //getUpdates($updateCounter);
-getUpdates($count);
-?>
--- jscript/staticVars.php
+++
@@ -1,3 +0,0 @@
-<?php
-$jquery_dir = 'C:\Apache\htdocs\jscript\jquery-1.7.1.js';
-?>
